b470acb77e7c3f3f5b6c51a981bb82d2c0cfc15e
maks0bs <maksthebro@173gmail.com>
2020-03-18 14:02:27 +0100

    testing without static resSend

--- app.js
+++ app.js
@@ -86,9 +86,9 @@ app.use(function (err, req, res, next){
 if (process.env.NODE_ENV === 'production') {
 	//set static folder
 	app.use(express.static('client/build'))
-	app.get('*', (req, res) => {
+	/*app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	})
+	})*/
 }
 
 let port = process.env.PORT || 8080;
